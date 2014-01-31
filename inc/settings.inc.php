9eec4d227d4971373dc727fb9639ddf6271a8fec
AntonLytkin <anton_lytkin@mail.ru>
2014-01-31 19:12:41 +0400

    settings

--- inc/settings.inc.php
+++ inc/settings.inc.php
@@ -2,14 +2,15 @@
 session_start();
 define('CLIENT_ID', '4148428'); // ID приложения
 define('CLIENT_SECRET', '7ihhqhbUsFiosQZ9VdWw'); // Защищённый ключ
-define('INDEX_URI', "http://$_SERVER[HTTP_HOST]/glitch/");
-define('REDIRECT_URI', "http://$_SERVER[HTTP_HOST]/glitch/get_token.php"); //
+define('INDEX_URI', "http://$_SERVER[HTTP_HOST]/glitch/"); // Адрес приложения
+define('REDIRECT_URI', "http://$_SERVER[HTTP_HOST]/glitch/get_token.php");
 define('VIEW_URI', "http://$_SERVER[HTTP_HOST]/glitch/view.php");
 define('URL', 'http://oauth.vk.com/authorize');
 define('TOKEN', 'https://oauth.vk.com/access_token');
 define('FRIENDS', 'https://api.vk.com/method/friends.get');
-define('F_COUNT', 25);
-define('PHOTO', 'photo_big');
+define('F_COUNT', 25); // Количество выводимых фотографий друзей
+//define('PHOTO', 'photo_100'); // Маленькие фотографии (необходимо раскомментировать)
+define('PHOTO', 'photo_big'); // Большие фотографии
 define('CONNECTION_ERROR', 'Невозможно подключиться к серверу vk!');
 define('TITLE', 'Глитч-арт аватарок друзей вконтакте!');
 define('FOOT_LINK', 'REGLITCH EM!');
